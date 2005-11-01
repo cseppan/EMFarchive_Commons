d56b66838232fde047ac37b3f3607c65cb6c6962
netswirl <netswirl>
2005-11-01 02:06:27 +0000

    Modified the test to use new Keyword and Keyword []

--- test/unit/gov/epa/emissions/commons/io/DatasetTypeTest.java
+++ test/unit/gov/epa/emissions/commons/io/DatasetTypeTest.java
@@ -3,23 +3,28 @@ package gov.epa.emissions.commons.io;
 import junit.framework.TestCase;
 
 public class DatasetTypeTest extends TestCase {
-
+    
     public void testAddKeyword() {
         DatasetType type = new DatasetType();
-        type.addKeyword("key1");
+        Keyword kw = new Keyword();
+        kw.setName("key1");
+        type.addKeyword(kw);
 
-        String[] actual = type.getKeywords();
+       Keyword[] actual = type.getKeywords();
         assertEquals(1, actual.length);
-        assertEquals("key1", actual[0]);
+        assertEquals("key1", actual[0].getName());
     }
 
     public void testSetKeywords() {
         DatasetType type = new DatasetType();
-        type.setKeywords(new String[]{"key1", "key2"});
+        Keyword kw1 = new Keyword("key1");
+        Keyword kw2 = new Keyword("key2");
         
-        String[] actual = type.getKeywords();
+        type.setKeywords(new Keyword[]{kw1, kw2});
+        
+        Keyword[] actual = type.getKeywords();
         assertEquals(2, actual.length);
-        assertEquals("key1", actual[0]);
-        assertEquals("key2", actual[1]);
+        assertEquals("key1", actual[0].getName());
+        assertEquals("key2", actual[1].getName());
     }
 }
