a7fa5c00107b1fbe56fc3a7875bdb0eecda6cc24
netswirl <netswirl>
2005-11-30 21:37:05 +0000

    added ScrollableRecordsStub back into commons

---
+++ test/integration/gov/epa/emissions/commons/db/ScrollableRecordsStub.java
@@ -0,0 +1,10 @@
+package gov.epa.emissions.commons.db;
+
+
+
+public class ScrollableRecordsStub extends ScrollableRecords {
+
+    public ScrollableRecordsStub() {
+        super(null, null);
+    }
+}
