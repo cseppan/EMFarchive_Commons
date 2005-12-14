e71697da0b9db992e0a2bbdb7c389920dcc12cd9
netswirl <netswirl>
2005-12-14 15:42:06 +0000

    added old methods with no sort order back in

--- src/gov/epa/emissions/commons/db/version/VersionedRecordsReader.java
+++ src/gov/epa/emissions/commons/db/version/VersionedRecordsReader.java
@@ -6,6 +6,10 @@ public interface VersionedRecordsReader {
 
     void close() throws SQLException;
 
+    VersionedRecord[] fetchAll(Version version, String table) throws SQLException;
+
+    ScrollableVersionedRecords fetch(Version version, String table) throws SQLException;
+
     VersionedRecord[] fetchAll(Version version, String table, String sortOrder) throws SQLException;
 
     ScrollableVersionedRecords fetch(Version version, String table, String sortOrder) throws SQLException;
