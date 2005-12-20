982f430dfac94fb4fa7b3f3e3d9316d3e935e043
netswirl <netswirl>
2005-12-20 19:07:13 +0000

    Chages to externals importer constructor

---
+++ test/integration/gov/epa/emissions/commons/io/externals/ShapeFileImporterTestCase.java
@@ -0,0 +1,43 @@
+package gov.epa.emissions.commons.io.externals;
+
+import gov.epa.emissions.commons.db.Datasource;
+import gov.epa.emissions.commons.db.DbServer;
+import gov.epa.emissions.commons.db.DbUpdate;
+import gov.epa.emissions.commons.db.SqlDataTypes;
+import gov.epa.emissions.commons.io.Dataset;
+import gov.epa.emissions.commons.io.SimpleDataset;
+import gov.epa.emissions.commons.io.importer.PersistenceTestCase;
+
+import java.util.Random;
+
+public abstract class ShapeFileImporterTestCase extends PersistenceTestCase {
+
+    private Datasource datasource;
+
+    private SqlDataTypes sqlDataTypes;
+
+    private Dataset dataset;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        DbServer dbServer = dbSetup.getDbServer();
+        sqlDataTypes = dbServer.getSqlDataTypes();
+        datasource = dbServer.getEmissionsDatasource();
+
+        dataset = new SimpleDataset();
+        dataset.setName("test");
+        dataset.setDatasetid(Math.abs(new Random().nextInt()));
+    }
+
+    protected void tearDown() throws Exception {
+        DbUpdate dbUpdate = new DbUpdate(datasource.getConnection());
+        dbUpdate.dropTable(datasource.getName(), dataset.getName());
+
+        dbUpdate.deleteAll(datasource.getName(), "versions");
+    }
+
+    public void testAssertsTrue() throws Exception {
+        assertTrue(true);
+    }
+}
