db2b14fe2c1a25884217da982fad01777c868001
netswirl <netswirl>
2005-12-21 15:33:41 +0000

    disable ShapeFilesImporterTest

--- test/integration/gov/epa/emissions/commons/io/externals/ShapeFileImporterTestCase.java
+++ test/integration/gov/epa/emissions/commons/io/externals/ShapeFileImporterTestCase.java
@@ -2,12 +2,13 @@ package gov.epa.emissions.commons.io.externals;
 
 import gov.epa.emissions.commons.db.Datasource;
 import gov.epa.emissions.commons.db.DbServer;
-import gov.epa.emissions.commons.db.DbUpdate;
 import gov.epa.emissions.commons.db.SqlDataTypes;
 import gov.epa.emissions.commons.io.Dataset;
 import gov.epa.emissions.commons.io.SimpleDataset;
+import gov.epa.emissions.commons.io.external.ShapeFilesImporter;
 import gov.epa.emissions.commons.io.importer.PersistenceTestCase;
 
+import java.io.File;
 import java.util.Random;
 
 public abstract class ShapeFileImporterTestCase extends PersistenceTestCase {
@@ -24,20 +25,28 @@ public abstract class ShapeFileImporterTestCase extends PersistenceTestCase {
         DbServer dbServer = dbSetup.getDbServer();
         sqlDataTypes = dbServer.getSqlDataTypes();
         datasource = dbServer.getEmissionsDatasource();
-
         dataset = new SimpleDataset();
-        dataset.setName("test");
+        dataset.setName("cnty_tn_01");
         dataset.setDatasetid(Math.abs(new Random().nextInt()));
     }
 
     protected void tearDown() throws Exception {
-        DbUpdate dbUpdate = dbSetup.dbUpdate(datasource);
-        dbUpdate.dropTable(datasource.getName(), dataset.getName());
-
-        dbUpdate.deleteAll(datasource.getName(), "versions");
+//        DbUpdate dbUpdate = new DbUpdate(datasource.getConnection());
+//        dbUpdate.deleteAll(datasource.getName(), "versions");
+ 
     }
 
-    public void testAssertsTrue() throws Exception {
+//public ShapeFilesImporter(File folder, String[] filePatterns, Dataset dataset, Datasource datasource, SqlDataTypes sqlDataType) throws ImporterException {
+
+    public void testShouldImportASmallAndSimpleNonPointFile() throws Exception {
+        String[] filePatterns = new String[]{"cnty_tn.*"};
+        
+        ShapeFilesImporter importer = new ShapeFilesImporter(new File("test/data/shape/cnty_tn"), filePatterns, dataset,datasource, sqlDataTypes);
+        
+        importer.run();
+
+        //dummy test
         assertTrue(true);
     }
+
 }
