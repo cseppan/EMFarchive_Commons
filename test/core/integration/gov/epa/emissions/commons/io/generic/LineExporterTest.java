6d415f108c7e5681acc9650a35fc27090cc1b4f7
qunhe <qunhe>
2005-12-30 16:09:04 +0000

    *** empty log message ***

--- test/core/integration/gov/epa/emissions/commons/io/generic/LineExporterTest.java
+++ test/core/integration/gov/epa/emissions/commons/io/generic/LineExporterTest.java
@@ -8,8 +8,15 @@ import gov.epa.emissions.commons.db.TableReader;
 import gov.epa.emissions.commons.io.Dataset;
 import gov.epa.emissions.commons.io.SimpleDataset;
 import gov.epa.emissions.commons.io.importer.PersistenceTestCase;
+import gov.epa.emissions.commons.io.importer.VersionedDataFormatFactory;
+import gov.epa.emissions.commons.io.importer.VersionedImporter;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class LineExporterTest extends PersistenceTestCase {
@@ -41,15 +48,64 @@ public class LineExporterTest extends PersistenceTestCase {
         LineImporter importer = new LineImporter(importFile, dataset, datasource, sqlDataTypes);
         importer.run();
 
-        LineExporter exporter = new LineExporter(dataset, datasource, new LineFileFormat(sqlDataTypes));
+        LineExporter exporter = new LineExporter(dataset, datasource, sqlDataTypes);
         File file = File.createTempFile("lineexporter", ".txt");
         exporter.export(file);
-        // FIXME: run the comparison tool, look at other exporter test
         assertEquals(22, countRecords());
+        
+        // assert records
+        List records = readData(file);
+
+        String expectedPattern1 = "#ORL";
+        String expectedPattern2 = "37119 0001 0001 1 1 'REXAMINC.;CUSTOMDIVISION' 40201301 02 01 60 7.5 375 2083.463 47.16 3083 0714 0 L _80.7081 35.12 17 108883 9.704141 _9 _9 _9 _9 _9";
+        String expectedPattern3 = "#Cutomized Division _ 1998";
+        String expectedPattern4 = "#End Comment";
+
+        assertEquals(expectedPattern1, records.get(0));
+        assertEquals(expectedPattern2, records.get(6));
+        assertEquals(expectedPattern3, records.get(15));
+        assertEquals(expectedPattern4, records.get(21));
+    }
+    
+    public void testExportVersionedSmallLineFile() throws Exception {
+        File importFile = new File("test/data/orl/nc", "small-point.txt");
+        LineImporter importer = new LineImporter(importFile, dataset, datasource, sqlDataTypes,
+                new VersionedDataFormatFactory(0));
+        VersionedImporter importer2 = new VersionedImporter(importer, dataset, datasource);
+        importer2.run();
+
+        LineExporter exporter = new LineExporter(dataset, datasource, sqlDataTypes, new VersionedDataFormatFactory(0));
+        File file = new File("C:\\lineexporter.txt");
+        exporter.export(file);
+        assertEquals(22, countRecords());
+        
+        // assert records
+        List records = readData(file);
+
+        String expectedPattern1 = "#ORL";
+        String expectedPattern2 = "37119 0001 0001 1 1 'REXAMINC.;CUSTOMDIVISION' 40201301 02 01 60 7.5 375 2083.463 47.16 3083 0714 0 L _80.7081 35.12 17 108883 9.704141 _9 _9 _9 _9 _9";
+        String expectedPattern3 = "#Cutomized Division _ 1998";
+        String expectedPattern4 = "#End Comment";
+
+        assertEquals(expectedPattern1, records.get(0));
+        assertEquals(expectedPattern2, records.get(6));
+        assertEquals(expectedPattern3, records.get(15));
+        assertEquals(expectedPattern4, records.get(21));
     }
 
     private int countRecords() {
         TableReader tableReader = tableReader(datasource);
         return tableReader.count(datasource.getName(), dataset.getName());
     }
+    
+    private List readData(File file) throws IOException {
+        List data = new ArrayList();
+
+        BufferedReader r = new BufferedReader(new FileReader(file));
+        for (String line = r.readLine(); line != null; line = r.readLine())
+            data.add(line);
+
+        return data;
+    }
+    
 }
