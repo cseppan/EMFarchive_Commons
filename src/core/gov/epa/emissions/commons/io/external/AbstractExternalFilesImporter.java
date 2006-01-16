b1016e3cae9a1b25a9542980aed2d2a5477d3a90
parthee <parthee>
2006-01-16 23:36:34 +0000

    external file importer is using FilePatternMatcher class to extract file names

--- src/core/gov/epa/emissions/commons/io/external/AbstractExternalFilesImporter.java
+++ src/core/gov/epa/emissions/commons/io/external/AbstractExternalFilesImporter.java
@@ -6,20 +6,15 @@ import gov.epa.emissions.commons.io.DataFormatFactory;
 import gov.epa.emissions.commons.io.Dataset;
 import gov.epa.emissions.commons.io.DatasetType;
 import gov.epa.emissions.commons.io.ExternalSource;
+import gov.epa.emissions.commons.io.importer.FilePatternMatcher;
 import gov.epa.emissions.commons.io.importer.Importer;
 import gov.epa.emissions.commons.io.importer.ImporterException;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 public abstract class AbstractExternalFilesImporter implements Importer {
-    private static Log log = LogFactory.getLog(AbstractExternalFilesImporter.class);
 
     protected String importerName;
 
@@ -49,17 +44,6 @@ public abstract class AbstractExternalFilesImporter implements Importer {
         }
     }
 
-    private File validateFile(File path, String fileName) throws ImporterException {
-        File file = new File(path, fileName);
-
-        if (!file.exists() || !file.isFile()) {
-            log.error("File " + file.getAbsolutePath() + " not found");
-            throw new ImporterException("File not found");
-        }
-
-        return file;
-    }
-
     private void init(File path, String[] filePatterns, Dataset dataset) throws ImporterException {
         DatasetType datasetType = dataset.getDatasetType();
         if (filePatterns.length > 1) {
@@ -78,55 +62,37 @@ public abstract class AbstractExternalFilesImporter implements Importer {
         }
     }
 
-    private File[] extractFileNames(File path, String fileName) throws ImporterException {
-        List allFiles = new ArrayList();
+    private File[] extractFileNames(File folder, String filePattern) throws ImporterException {
+        String[] fileNamesInFolder = fileNames(folder);
 
-        File[] files = null;
-
-        if (fileName.indexOf("*") >= 0) {
-            String[] allFilesInFolder = path.list();
-            Pattern pat = Pattern.compile(createPattern(fileName));
-            for (int i = 0; i < allFilesInFolder.length; i++) {
-                String ipFile = allFilesInFolder[i];
-                Matcher m = pat.matcher(ipFile);
-                if (m.matches()) {
-                    allFiles.add(validateFile(path, ipFile));
-                }
-            }
-        } else {
-            allFiles.add(validateFile(path, fileName));
+        FilePatternMatcher matcher = new FilePatternMatcher(filePattern);
+        String[] matchedFileNames = matcher.matchingNames(fileNamesInFolder);
+        if (matchedFileNames == null || matchedFileNames.length == 0) {
+            throw new ImporterException("There are no files found in the directory '" + folder.getAbsolutePath()
+                    + "' matching pattern '" + filePattern + "'");
         }
-        files = (File[]) allFiles.toArray(new File[allFiles.size()]);
-
+        File[] files = new File[matchedFileNames.length];
+        for (int i = 0; i < files.length; i++) {
+            files[i] = new File(folder, matchedFileNames[i]);
+        }
         return files;
     }
 
-    private String createPattern(String inputString) {
-        String OPEN_QUOTE = "\\Q";
-        String END_QUOTE = "\\E";
-        String dotPattern = OPEN_QUOTE + "." + END_QUOTE;
-        String asterixPattern = ".*";
-        String inputPattern = null;
-
-        int dotLocn = inputString.indexOf(".");
-        inputPattern = inputString.substring(0, dotLocn) + dotPattern
-                + inputString.substring(dotLocn + 1, inputString.length());
-
-        String outputPattern = "";
-        String holdPattern = inputPattern;
-        boolean done = false;
-        while (!done) {
-            int asterixPos = holdPattern.indexOf("*");
-
-            if (asterixPos < 0) {
-                done = true;
-            } else {
-                outputPattern = outputPattern + holdPattern.substring(0, asterixPos) + asterixPattern;
-                holdPattern = holdPattern.substring(asterixPos + 1, holdPattern.length());
+    private String[] fileNames(File folder) throws ImporterException {
+        if (!folder.isDirectory()) {
+            throw new ImporterException(folder.getAbsolutePath() + " is not a directory");
+        }
+        List names = new ArrayList();
+        String[] fileNames = folder.list();
+        for (int i = 0; i < fileNames.length; i++) {
+            if (new File(folder, fileNames[i]).isFile()) {
+                names.add(fileNames[i]);
             }
         }
-
-        return outputPattern;
+        if (names.isEmpty()) {
+            throw new ImporterException("There are no files in the directory '" + folder + "'");
+        }
+        return (String[]) names.toArray(new String[0]);
     }
 
 }
