eb00ffd63bd5fc06796a88fb4b97530f4c4c974b
rhavaldar <rhavaldar>
2006-02-23 17:51:39 +0000

    added bean-style methods

--- src/core/gov/epa/emissions/commons/io/TableMetadata.java
+++ src/core/gov/epa/emissions/commons/io/TableMetadata.java
@@ -1,6 +1,7 @@
 package gov.epa.emissions.commons.io;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import gov.epa.emissions.commons.io.ColumnMetaData;
@@ -8,19 +9,19 @@ import gov.epa.emissions.commons.io.ColumnMetaData;
 public class TableMetadata {
 
     private String table;
+
     private List cols;
 
     public TableMetadata() {
-        super();
         cols = new ArrayList();
     }
 
-    public TableMetadata(String table){
+    public TableMetadata(String table) {
         this();
-        this.table= table;
+        this.table = table;
     }
 
-    public void addColumnMetaData(ColumnMetaData col){
+    public void addColumnMetaData(ColumnMetaData col) {
         cols.add(col);
     }
 
@@ -34,6 +35,10 @@ public class TableMetadata {
 
     public void setTable(String table) {
         this.table = table;
+    }
+
+    public void setCols(ColumnMetaData[] cols) {
+        this.cols = Arrays.asList(cols);
     }
 
 }
