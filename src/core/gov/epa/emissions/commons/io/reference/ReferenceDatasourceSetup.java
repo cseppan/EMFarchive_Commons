86d6cce9e803c61941fcb2d8c1b3ffb1d65ba4f0
parthee <parthee>
2006-12-04 20:37:00 +0000

    added notes to update the reference schema at EPA

--- src/core/gov/epa/emissions/commons/io/reference/ReferenceDatasourceSetup.java
+++ src/core/gov/epa/emissions/commons/io/reference/ReferenceDatasourceSetup.java
@@ -5,6 +5,21 @@ import gov.epa.emissions.commons.db.DatabaseSetup;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
+/*NOTES for updating a reference schema after updating a reference file
+1. Drop your reference schema - DROP SCHEMA reference CASCADE;
+2. Create a reference schema - CREATE SCHEMA reference AUTHORIZATION emf;
+3. Run the ant target called  'setup-reference-schema'
+4. Backup the reference schema using pgadmin
+   a. Right click on the reference schema and select 'Backup'
+   b. A dialog will applear 
+       - specify a file name 
+       - select check box next 'insert commands'
+       - select the radio button next to 'PLAIN'
+       click 'OK
+         
+5. Drop the reference schema at EPA
+6. Use the back up file to create the reference schema and tables using pgadmin or psql 
+*/
 
 public class ReferenceDatasourceSetup {
 
