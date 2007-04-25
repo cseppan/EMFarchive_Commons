b47c430bf578a97681285d0f7a10d10b2cd77878
qunhe <qunhe>
2007-04-25 16:21:32 +0000

    Added a 'Label' column to smoke report file format for import.

--- src/core/gov/epa/emissions/commons/io/other/SMKReportFileFormat.java
+++ src/core/gov/epa/emissions/commons/io/other/SMKReportFileFormat.java
@@ -34,13 +34,14 @@ public class SMKReportFileFormat implements FileFormat, DelimitedFileFormat {
     
     private Map createMap(SqlDataTypes types) {
         HashMap map = new HashMap();
-        map.put("Date", new Column("DATE", types.stringType(10), 10, new StringFormatter(10)));
-        map.put("State", new Column("STATE", types.stringType(50), 50, new StringFormatter(50)));
-        map.put("Region", new Column("REGION", types.stringType(10), 10, new StringFormatter(10)));
-        map.put("Hour", new Column("HOUR", types.intType(), new IntegerFormatter()));
+        map.put("DATE", new Column("DATE", types.stringType(10), 10, new StringFormatter(10)));
+        map.put("STATE", new Column("STATE", types.stringType(50), 50, new StringFormatter(50)));
+        map.put("REGION", new Column("REGION", types.stringType(10), 10, new StringFormatter(10)));
+        map.put("HOUR", new Column("HOUR", types.intType(), new IntegerFormatter()));
         map.put("SCC", new Column("SCC", types.stringType(10), 10, new StringFormatter(10)));
-        map.put("SCC Description", new Column("SCCDESC", types.stringType(256), 256, new StringFormatter(258)));
-        map.put("County", new Column("COUNTY", types.stringType(50), 50, new StringFormatter(50)));
+        map.put("SCC DESCRIPTION", new Column("SCCDESC", types.stringType(256), 256, new StringFormatter(258)));
+        map.put("COUNTY", new Column("COUNTY", types.stringType(50), 50, new StringFormatter(50)));
+        map.put("LABEL", new Column("LABEL", types.stringType(128), 50, new StringFormatter(128)));
         
         return map;
     }
@@ -49,7 +50,7 @@ public class SMKReportFileFormat implements FileFormat, DelimitedFileFormat {
         List base = new ArrayList();
         
         for(int i = 0; i < cols.length; i++) {
-            Column col = (Column)map.get(cols[i].trim());           
+            Column col = (Column)map.get(cols[i].trim().toUpperCase());           
             if(col != null)
                 base.add(i, col);
             else
