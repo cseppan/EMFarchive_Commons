c21e4367f392c4822097e4b16c6cb1cb45dc5348
qunhe <qunhe>
2008-07-15 18:06:21 +0000

    Modified the realformatter to consider the data value is longer than the predefined width of the value.

--- src/core/gov/epa/emissions/commons/io/RealFormatter.java
+++ src/core/gov/epa/emissions/commons/io/RealFormatter.java
@@ -8,11 +8,11 @@ import corejava.Format;
 public class RealFormatter implements ColumnFormatter {
 
     public final Format FORMAT = new Format("%14.7e");
-    
+
     private int spaces;
-    
+
     private int width;
-    
+
     public RealFormatter(int width, int spaces) {
         this.spaces = spaces;
         this.width = width;
@@ -26,14 +26,22 @@ public class RealFormatter implements ColumnFormatter {
     public String format(String name, ResultSet data) throws SQLException {
         if (data.getString(name) == null || data.getFloat(name) == -9)
             return getSpaces(this.width + this.spaces);
-        
-        int prefix = this.width - data.getString(name).length();
 
-        return getSpaces(prefix) + data.getString(name) + getSpaces(this.spaces);
+        String dataValue = data.getString(name);
+        int dataWidth = dataValue.length();
+
+        if (dataWidth < this.width) {
+            int prefix = this.width - dataWidth;
+            return getSpaces(prefix) + dataValue + getSpaces(this.spaces);
+        }
+        
+        if (dataWidth == this.width)
+            return dataValue;
+        
+        return dataValue.substring(0, this.width);
     }
-    
-    public String getSpaces(int n)
-    {
+
+    public String getSpaces(int n) {
         StringBuffer buf = new StringBuffer();
         for (int i = 0; i < n; i++)
             buf.append(" ");
