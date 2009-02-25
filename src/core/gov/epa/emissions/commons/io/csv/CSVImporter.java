d7e797bbc69f185fc51c8ecda36cca3e2dff1e47
qunhe <qunhe>
2009-02-25 21:34:19 +0000

    Consolidated csv imported data; modified the dataset delete/purge functions to acomodate the changes.

--- src/core/gov/epa/emissions/commons/io/csv/CSVImporter.java
+++ src/core/gov/epa/emissions/commons/io/csv/CSVImporter.java
@@ -65,11 +65,10 @@ public class CSVImporter implements Importer {
 
     public void run() throws ImporterException {
         DataTable dataTable = new DataTable(dataset, datasource);
-        String table = dataTable.name();
+        String table = null;
 
         try {
-            if (!dataTable.exists(table))
-                dataTable.create(formatUnit.tableFormat());
+            table = dataTable.createConsolidatedTable(formatUnit.tableFormat());
             doImport(file, dataset, table, formatUnit.tableFormat());
         } catch (Exception e) {
             e.printStackTrace();
