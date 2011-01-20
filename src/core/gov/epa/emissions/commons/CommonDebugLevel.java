b75642d3f8033c62b5f0b1161ead26f81ff86d24
jizhenz <jizhenz>
2011-01-20 21:22:53 +0000

    g.	Show hour glass cursor after clicking control measure Import button, restore the cursor after the confirm dialog shown. h.	Suppress logging status messages in control measure importing window when parsing summary file to report how many measures will be imported. i.	When reporting control technologies affected, make sure only reported the ones affected. j.	A related problem shown after purging part of the control measures that a control strategy or control program has  the strategy or program manager window cannot be shown. Identify the reason: some of the control measures returned from a strategy or program are null. Fix this from the client side  remove null control measures when construct control measure tab of the strategy or program manager. k.	Report the abbreviation and name of the control measures that to be removed in Import Status window. l.	Report control the names of the strategies affected and the names of the programs affected in the Import Status Window.

--- src/core/gov/epa/emissions/commons/CommonDebugLevel.java
+++ src/core/gov/epa/emissions/commons/CommonDebugLevel.java
@@ -5,5 +5,5 @@ public class CommonDebugLevel {
     public static final boolean DEBUG_PAGE = false;
     public static final boolean DEBUG_PAGE_2 = false;
     public static final boolean DEBUG_PAGE_3 = false;
-
+    public static final boolean DEBUG_CMIMPORT = false;
 }
