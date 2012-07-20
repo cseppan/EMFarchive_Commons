bac513219519266bffb065071ed56e782c5df233
jizhenz <jizhenz>
2012-07-20 15:17:49 +0000

    Solve the problem: can not import after moving the versions table from emissions to emf

--- src/core/gov/epa/emissions/commons/io/importer/VersionedImporter.java
+++ src/core/gov/epa/emissions/commons/io/importer/VersionedImporter.java
@@ -3,12 +3,12 @@ package gov.epa.emissions.commons.io.importer;
 import gov.epa.emissions.commons.data.Dataset;
 import gov.epa.emissions.commons.db.Datasource;
 import gov.epa.emissions.commons.db.DbServer;
-import gov.epa.emissions.commons.db.version.Version;
-import gov.epa.emissions.commons.util.utils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Date;
-
-import org.hibernate.Session;
 
 public class VersionedImporter implements Importer {
     private Importer delegate;
@@ -18,15 +18,15 @@ public class VersionedImporter implements Importer {
     private Datasource datasource;
 
     private Date lastModifiedDate;
-    
+
     public VersionedImporter(Importer delegate, Dataset dataset, DbServer dbServer, Date lastModifiedDate) {
         this.delegate = delegate;
         this.dataset = dataset;
         this.datasource = dbServer.getEmissionsDatasource(); 
-// completed: datasource is not used anywhere else except addVersionZeroEntryToVersionsTable
+        // completed: datasource is not used anywhere else except addVersionZeroEntryToVersionsTable
         this.lastModifiedDate = lastModifiedDate;
     }
-    
+
     //NOTE: need to access the importer to get external sources
     public Importer getWrappedImporter() {
         return delegate;
@@ -39,39 +39,44 @@ public class VersionedImporter implements Importer {
         } catch (Exception e) {
             throw new ImporterException("Could not add Version Zero entry to the Versions Table." + e.getMessage());
         }
-//NOTE: should let the calling function to close it explicitly for ease of db management 2/13/2008
-//        } finally {
-//            try {
-//                this.dbServer.disconnect();
-//            } catch (Exception exc) {
-//                throw new ImporterException("Could not disconnect db server: " + exc.getMessage());
-//            }
-//        }
+        //NOTE: should let the calling function to close it explicitly for ease of db management 2/13/2008
+        //        } finally {
+        //            try {
+        //                this.dbServer.disconnect();
+        //            } catch (Exception exc) {
+        //                throw new ImporterException("Could not disconnect db server: " + exc.getMessage());
+        //            }
+        //        }
     }
-    
+
     private void addVersionZeroEntryToVersionsTable(Datasource datasource, Dataset dataset) throws Exception {
 
-//      TableModifier modifier = new TableModifier(datasource,"versions"); 
-//      String[] data = { null, dataset.getId() + "", "0", "Initial Version", "", "true", new Timestamp(lastModifiedDate.getTime())+""};
-//        modifier.insertOneRow(data);
-        
-        
-        Session session = utils.getHibernateSession();
-
-        Version defaultZeroVersion = new Version(0);
-        defaultZeroVersion.setName("Initial Version");
-        defaultZeroVersion.setPath("");
-        defaultZeroVersion.setDatasetId(dataset.getId());
-        defaultZeroVersion.setLastModifiedDate(lastModifiedDate);
-        defaultZeroVersion.setFinalVersion(true);
-        defaultZeroVersion.setDescription("");
-        
+        String sql =
+            "INSERT INTO emf.versions " +
+            "(dataset_id, version, name, path, final_version, date, description) " +
+            "VALUES (?,?,?,?,?,?,?)";
+        Connection connection = null;
+        PreparedStatement insertStatement = null;
         try {
-            utils.add(defaultZeroVersion, session);
-        } catch (Exception e) {
-            throw new Exception("Could not add version 0 entry for dataset " + dataset.getName() + " into versions table: " + e.getMessage());
+            connection = datasource.getConnection();
+            insertStatement = connection.prepareStatement(sql);
+            insertStatement.setInt(1, dataset.getId());
+            insertStatement.setInt(2, 0);
+            insertStatement.setString(3, "Initial Version");
+            insertStatement.setString(4, "");
+            insertStatement.setBoolean(5, true);
+            insertStatement.setTimestamp(6, new Timestamp(lastModifiedDate.getTime()));
+            insertStatement.setString(7, "");
+            insertStatement.execute();
+        } catch (SQLException e) {
+            throw e;
         } finally {
-            session.close();
+            if ( insertStatement != null) {
+                insertStatement.close();
+            }
+            if ( connection != null) {
+                connection = null;
+            }
         }
     }
 }
